a4b71e503731a51122dfe9c24f8022452629d774
juanvelozo <juanvelozomoreno18@gmail.com>
2026-02-13 19:35:43 -0300

    anima card con animate presence

--- src/components/ui/Card/Card.tsx
+++ src/components/ui/Card/Card.tsx
@@ -1,30 +1,49 @@
 import { memo, type ReactNode } from "react";
+import { AnimatePresence, motion } from "framer-motion";
 
 interface CardProps {
   title?: ReactNode;
   children: ReactNode;
   className?: string;
   cta?: ReactNode;
+  animationDelay?: number;
 }
 
-const Card = memo(function Card({ title, children, className = "", cta }: CardProps) {
+const Card = memo(function Card({
+  title,
+  children,
+  className = "",
+  cta,
+  animationDelay = 0,
+}: CardProps) {
   return (
-    <div
-      className={`rounded-3xl z-0  border border-gray-200 bg-white/50 p-6 dark:border-gray-700 backdrop-blur-lg dark:bg-gray-800 ${className}`}
-    >
-      {(title || cta) && <div className="flex items-center justify-between  mb-5">
-
-        {title && (
-          <div className="select-none font-stretch-110% text-xl font-semibold text-gray-900 dark:text-white">
-            {title}
+    <AnimatePresence>
+      <motion.div
+        key="card"
+        initial={{ scale: 0.95, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        exit={{ scale: 0.95, opacity: 0 }}
+        transition={{
+          type: "spring",
+          stiffness: 400,
+          damping: 30,
+          delay: animationDelay,
+        }}
+        className={`rounded-3xl z-0 border border-gray-200 bg-white/50 p-6 dark:border-gray-700 backdrop-blur-lg dark:bg-gray-800 ${className}`}
+      >
+        {(title || cta) && (
+          <div className="flex items-center justify-between mb-5">
+            {title && (
+              <div className="select-none font-stretch-110%  text-xl font-semibold text-gray-900 dark:text-white">
+                {title}
+              </div>
+            )}
+            {cta && cta}
           </div>
         )}
-        {cta && (
-          cta
-        )}
-      </div>}
-      {children}
-    </div>
+        {children}
+      </motion.div>
+    </AnimatePresence>
   );
 });
 
